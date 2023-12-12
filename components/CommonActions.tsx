a0642f0d4c0c2877af7a3ce29cbbd57f7b0347f5
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2023-12-12 17:29:42 -0600

    remodeled the feature cards and network configuration information and moved both to the top of homepage,config is now a collapsable element and also moved up in sidebar.

---
+++ components/CommonActions.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import Link from "next/link";
+import {
+  ChartBarIcon,
+  BookOpenIcon,
+  ServerIcon,
+  BeakerIcon,
+} from "@heroicons/react/outline";
+import styles from "./CommonActions.module.css";
+import { FaGithub } from "react-icons/fa";
+
+const features = [
+  {
+    Icon: ServerIcon,
+    title: "Node Deployment",
+    description: `Want to spin up a full node on the Tangle Network? We've made it easier than ever!`,
+    href: "/docs/tangle-network/node/docker-node",
+    action: "Deploy a Docker Node",
+  },
+  {
+    Icon: ServerIcon,
+    title: "Validators",
+    description: `Start your journey on Tangle Network. This guide will walk you through the steps to become a validator, ensuring network security and integrity.`,
+    href: "/docs/tangle-network/node/quickstart/",
+    action: "Launch a Quick Validator Node",
+  },
+  {
+    Icon: FaGithub,
+    title: "Tangle Open Source",
+    description: `Multy-party threshold ECDSA (GG20) Substrate node`,
+    href: "https://github.com/webb-tools/tangle",
+    action: "View the Repo",
+  },
+  {
+    Icon: ServerIcon,
+    title: "Accounts",
+    description: `Tangle uses Polkadot Apps to manage Accounts.`,
+    href: "https://polkadot.js.org/apps/?rpc=wss://rpc.tangle.tools#/explorer",
+    action: "Go to Polkadot Apps",
+  },
+  {
+    Icon: BookOpenIcon,
+    title: "Staking",
+    description: `Through Polkadot Apps you can create `,
+    href: "https://polkadot.js.org/apps/?rpc=wss://rpc.tangle.tools#/democracy",
+    action: "Manage Staking",
+  },
+  {
+    Icon: BookOpenIcon,
+    title: "Governance",
+    description: `Through governance, you can create proposals for updating cross-chain applications.`,
+    href: "https://polkadot.js.org/apps/?rpc=wss://rpc.tangle.tools#/democracy",
+    action: "Interact with Governance",
+  },
+  {
+    Icon: BeakerIcon,
+    title: "Faucet",
+    description: `Our easy-to-use testnet faucet allows you to claim test tokens with just a few clicks. Start experimenting with Hubble Bridge today.`,
+    href: "https://faucet.tangle.tools",
+    action: "Go to Faucet",
+  },
+  {
+    Icon: ChartBarIcon,
+    title: "DKG Stats dApp",
+    description: `The DKG Stats dApp provides insights into the DKG protocol running on the Tangle Network.`,
+    href: "https://stats.tangle.tools",
+    action: "View DKG Functionality",
+  },
+];
+
+const Card = ({ Icon, title, description, href, action }) => (
+  <div className={styles.card}>
+    <div className={styles.cardHeader}>
+      <h3 className={styles.title}>{title}</h3>
+    </div>
+    {/*}   <p className={styles.description}>{description}</p>
+    {/* Wrap the entire footer content with Link */}
+    <Link href={href} passHref className={styles.cardFooter}>
+      <div className={styles.footerContent}>
+        <Icon className={styles.icon} aria-hidden="true" />
+        {action}
+      </div>
+      {/* Arrow */}
+      <span className={styles.cardArrow}>→</span>
+    </Link>
+  </div>
+);
+
+// CommonActions component that renders a grid of Cards
+export const CommonActions = () => {
+  return (
+    <div className={styles.actionCardGrid}>
+      {features.map((feature, index) => (
+        <Card key={index} {...feature} />
+      ))}
+    </div>
+  );
+};
+
+export default CommonActions;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles.css";
 import "../custom.css";
-
 import { SSRProvider } from "@react-aria/ssr";
 import type { AppProps } from "next/app";
 import type { ReactNode } from "react";
@@ -17,32 +16,37 @@ if (typeof window !== "undefined" && !("requestIdleCallback" in window)) {
   // @ts-ignore
   window.requestIdleCallback = (fn) => setTimeout(fn, 1);
   // @ts-ignore
-  window.cancelIdleCallback = (e) => clearTimeout(e);
+  window.cancelIdleCallback = (id) => clearTimeout(id);
 }
 
 export default function Nextra({ Component, pageProps }: NextraAppProps) {
+  // Define a layout if it doesn't exist in the page component
+  const getLayout = Component.getLayout || ((page) => page);
+
   return (
     <SSRProvider>
-      <>
-        {/**
-         * Globally defined svg linear gradient, for use in icons
-         */}
-        <svg height="0px" width="0px">
-          <defs>
-            <linearGradient
-              id="pink-gradient"
-              x1="0%"
-              y1="0%"
-              x2="100%"
-              y2="100%"
-            >
-              <stop offset="0%" stopColor="rgba(156, 81, 161, 1)" />
-              <stop offset="70%" stopColor="rgba(255, 30, 86, 1)" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </>
-      <Component {...pageProps} />
+      <svg height="0px" width="0px" style={{ position: "absolute" }}>
+        {" "}
+        {/* Adjust style as needed */}
+        <defs>
+          <linearGradient
+            id="pink-gradient"
+            x1="0%"
+            y1="0%"
+            x2="100%"
+            y2="100%"
+          >
+            <stop offset="0%" stopColor="rgba(156, 81, 161, 1)" />
+            <stop offset="70%" stopColor="rgba(255, 30, 86, 1)" />
+          </linearGradient>
+        </defs>
+      </svg>
+
+      {getLayout(
+        <>
+          <Component {...pageProps} />
+        </>
+      )}
     </SSRProvider>
   );
 }
