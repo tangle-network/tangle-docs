934e3a1a6872c2cfae4f1c2cccbd13e6f2e65811
Thom Ivy <38070512+thomivy@users.noreply.github.com>
2024-03-11 12:20:14 -0500

    updated allocation table

--- components/AllocationTable.tsx
+++ components/AllocationTable.tsx
@@ -70,20 +70,20 @@ export default function AllocationTable() {
           <Row>
             <Cell>Contributors</Cell>
             <Cell>Webb (Developer)</Cell>
-            <Cell>28.56%</Cell>
-            <Cell>B-Vesting</Cell>
+            <Cell>28.56500%</Cell>
+            <Cell>A-Vesting</Cell>
             <Cell>12</Cell>
             <Cell>24</Cell>
             <Cell>0%</Cell>
             <Cell>0.00</Cell>
-            <Cell>13,562,500.00</Cell>
-            <Cell>2,260,416.67</Cell>
-            <Cell>27,125,000.00</Cell>
+            <Cell>14,282,500.00</Cell>
+            <Cell>2,380,416.67</Cell>
+            <Cell>28,565,000.00</Cell>
           </Row>
           <Row>
             <Cell>Contributors</Cell>
             <Cell>Investors</Cell>
-            <Cell>13.64%</Cell>
+            <Cell>13.64000%</Cell>
             <Cell>A-Vesting</Cell>
             <Cell>12</Cell>
             <Cell>24</Cell>
@@ -95,31 +95,32 @@ export default function AllocationTable() {
           </Row>
           <Row>
             <Cell>Contributors</Cell>
-            <Cell>Indiv. Webb Contributors</Cell>
-            <Cell>1.44%</Cell>
-            <Cell>B-Vesting</Cell>
+            <Cell>Indiv. Contributors</Cell>
+            <Cell>1.43500%</Cell>
+            <Cell>A-Vesting</Cell>
             <Cell>12</Cell>
-            <Cell>48</Cell>
+            <Cell>24</Cell>
             <Cell>0%</Cell>
             <Cell>0.00</Cell>
-            <Cell>358,750.00</Cell>
-            <Cell>39,861.11</Cell>
+            <Cell>717,500.00</Cell>
+            <Cell>119,583.33</Cell>
             <Cell>1,435,000.00</Cell>
           </Row>
           <SubtotalRow>
             <Cell colSpan={2}>
-              <strong>Contributors Subtotal</strong>
+              <strong>Contributors Total</strong>
             </Cell>
-            <Cell>43.64%</Cell>
+            <Cell>43.64000%</Cell>
             <Cell colSpan={4}></Cell>
-            <Cell>1,356,250.00</Cell>
-            <Cell colSpan={2}></Cell>
-            <Cell>42,200,000.00</Cell>
+            <Cell>0.00</Cell>
+            <Cell>21,820,000.00</Cell>
+            <Cell colSpan={1}></Cell>
+            <Cell>43,640,000.00</Cell>
           </SubtotalRow>
           <Row>
             <Cell>Governance-Managed</Cell>
-            <Cell>Treasury</Cell>
-            <Cell>36.36%</Cell>
+            <Cell>On-chain Treasury</Cell>
+            <Cell>36.36000%</Cell>
             <Cell>n/a</Cell>
             <Cell>n/a</Cell>
             <Cell>n/a</Cell>
@@ -132,93 +133,97 @@ export default function AllocationTable() {
           <Row>
             <Cell>Governance-Managed</Cell>
             <Cell>Foundation</Cell>
-            <Cell>15.00%</Cell>
-            <Cell>n/a</Cell>
-            <Cell>n/a</Cell>
-            <Cell>n/a</Cell>
-            <Cell>100%</Cell>
-            <Cell>n/a</Cell>
-            <Cell>n/a</Cell>
-            <Cell>n/a</Cell>
+            <Cell>15.00000%</Cell>
+            <Cell>A-Vesting</Cell>
+            <Cell>12</Cell>
+            <Cell>24</Cell>
+            <Cell>5%</Cell>
+            <Cell>750,000.00</Cell>
+            <Cell>7,496,250.00</Cell>
+            <Cell>1,249,375.00</Cell>
             <Cell>15,000,000.00</Cell>
           </Row>
           <SubtotalRow>
             <Cell colSpan={2}>
-              <strong>Governance-Managed Subtotal</strong>
+              <strong>Governance-Managed Total</strong>
             </Cell>
-            <Cell>51%</Cell>
-            <Cell colSpan={7}></Cell>
+            <Cell>51.36000%</Cell>
+            <Cell colSpan={4}></Cell>
+            <Cell>750,000.00</Cell>
+            <Cell>7,496,250.00</Cell>
+            <Cell colSpan={1}></Cell>
             <Cell>51,360,000.00</Cell>
           </SubtotalRow>
           <Row>
             <Cell>Airdrops</Cell>
             <Cell>Leaderboard Participants</Cell>
-            <Cell>2%</Cell>
-            <Cell>C-Vesting</Cell>
+            <Cell>2.00000%</Cell>
+            <Cell>B-Vesting</Cell>
             <Cell>1</Cell>
             <Cell>24</Cell>
             <Cell>5%</Cell>
             <Cell>100,000.00</Cell>
-            <Cell>79,166.67</Cell>
-            <Cell>82,608.70</Cell>
+            <Cell>83,291.67</Cell>
+            <Cell>86,913.04</Cell>
             <Cell>2,000,000.00</Cell>
           </Row>
           <Row>
             <Cell>Airdrops</Cell>
             <Cell>DOT Validators Snapshot</Cell>
-            <Cell>1%</Cell>
-            <Cell>C-Vesting</Cell>
+            <Cell>1.00000%</Cell>
+            <Cell>B-Vesting</Cell>
             <Cell>1</Cell>
             <Cell>24</Cell>
             <Cell>5%</Cell>
             <Cell>50,000.00</Cell>
-            <Cell>39,583.33</Cell>
-            <Cell>41,304.35</Cell>
+            <Cell>41,645.83</Cell>
+            <Cell>43,456.52</Cell>
             <Cell>1,000,000.00</Cell>
           </Row>
           <Row>
             <Cell>Airdrops</Cell>
             <Cell>EDG Genesis Participants</Cell>
-            <Cell>1%</Cell>
-            <Cell>C-Vesting</Cell>
+            <Cell>1.00000%</Cell>
+            <Cell>B-Vesting</Cell>
             <Cell>1</Cell>
             <Cell>24</Cell>
             <Cell>5%</Cell>
             <Cell>50,000.00</Cell>
-            <Cell>39,583.33</Cell>
-            <Cell>41,304.35</Cell>
+            <Cell>41,645.83</Cell>
+            <Cell>43,456.52</Cell>
             <Cell>1,000,000.00</Cell>
           </Row>
           <Row>
             <Cell>Airdrops</Cell>
             <Cell>EDG 2023 Snapshot</Cell>
-            <Cell>1%</Cell>
-            <Cell>C-Vesting</Cell>
+            <Cell>1.00000%</Cell>
+            <Cell>B-Vesting</Cell>
             <Cell>1</Cell>
             <Cell>24</Cell>
             <Cell>5%</Cell>
             <Cell>50,000.00</Cell>
-            <Cell>39,583.33</Cell>
-            <Cell>41,304.35</Cell>
+            <Cell>41,645.83</Cell>
+            <Cell>43,456.52</Cell>
             <Cell>1,000,000.00</Cell>
           </Row>
           <SubtotalRow>
             <Cell colSpan={2}>
               <strong>Airdrops Subtotal</strong>
             </Cell>
-            <Cell colSpan={5}></Cell>
+            <Cell>5.00000%</Cell>
+            <Cell colSpan={4}></Cell>
             <Cell>250,000.00</Cell>
-            <Cell>197,916.67</Cell>
-            <Cell colSpan={1}></Cell>
+            <Cell>208,229.17</Cell>
+            <Cell>217,282.61</Cell>
             <Cell>5,000,000.00</Cell>
           </SubtotalRow>
           <TotalRow>
             <Cell colSpan={2}>
               <strong>Total Supply</strong>
             </Cell>
-            <Cell>100%</Cell>
+            <Cell>100.00000%</Cell>
             <Cell colSpan={4}></Cell>
-            <Cell>1,678,000.00</Cell>
+            <Cell>1,000,000.00</Cell>
             <Cell colSpan={2}></Cell>
             <Cell>100,000,000.00</Cell>
           </TotalRow>
