aec689972b71a15f673caa15eb5a30422a575b67
Thom Ivy <thom.ivy.1@gmail.com>
2024-05-08 20:39:59 -0500

    replaced navbar socials with launch app button

--- theme.config.tsx
+++ theme.config.tsx
@@ -8,6 +8,23 @@ import { Discord, Github, Twitter } from "./components/Social";
 
 const SITE_ROOT = "https://docs.tangle.tools";
 
+const LaunchAppButton = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    window.open("http://app.tangle.tools/", "_blank");
+  };
+  return (
+    <button
+      type="button"
+      className="text-white !bg-gradient-to-br align-middle mt-2 ml-12 from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+      onClick={handleClick}
+    >
+      Launch App
+    </button>
+  );
+};
+
 const theme: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/webb-tools/tangle-docs/tree/main",
   useNextSeoProps() {
@@ -104,9 +121,7 @@ const theme: DocsThemeConfig = {
     component: Navigation,
     extraContent: (
       <>
-        <Github />
-        <Discord />
-        <Twitter />
+        <LaunchAppButton />
       </>
     ),
   },
