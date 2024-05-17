85ef87d04e9e0499a3c9657e1f8123abdd81ea90
Thom Ivy <thom.ivy.1@gmail.com>
2024-05-17 17:22:09 -0500

    attempts to fix build error

--- components/YoutubeVideo.tsx
+++ components/YoutubeVideo.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import LiteYouTubeEmbed from "react-lite-youtube-embed";
 import "react-lite-youtube-embed/dist/LiteYouTubeEmbed.css";
 
-const YouTubeVideo = ({ id, title }) => {
+const YoutubeVideo = ({ id, title }) => {
   return <LiteYouTubeEmbed id={id} title={title} poster="maxresdefault" webp />;
 };
 
-export default YouTubeVideo;
+export default YoutubeVideo;
