57272c6a1121092dcbb2662bbbab404dda2f4955
Trung-Tin Pham <60747384+AtelyPham@users.noreply.github.com>
2024-07-22 23:53:47 +0700

    fix: Display theme & icon conditionally based on breakpoints

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,6 +9,73 @@ import { ThemeSwitch } from "nextra-theme-docs";
 
 const SITE_ROOT = "https://docs.tangle.tools";
 
+const Head = () => {
+  const router = useRouter();
+  const fullUrl =
+    router.asPath === "/" ? SITE_ROOT : `${SITE_ROOT}${router.asPath}`;
+
+  const { frontMatter } = useConfig();
+
+  // Set default values for title and description
+  const defaultTitle = "Tangle Tools Documentation";
+  const defaultDescription = "Comprehensive documentation for Tangle Tools";
+
+  // Get the title and description from the front matter, or use the default values
+  const title = frontMatter.title || defaultTitle;
+  const description = frontMatter.description || defaultDescription;
+
+  return (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href={`/images/favicon/apple-touch-icon.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href={`/images/favicon/favicon-32x32.png`}
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href={`/images/favicon/favicon-16x16.png`}
+      />
+      <link
+        rel="mask-icon"
+        href={`/images/favicon/safari-pinned-tab.svg`}
+        color="#000000"
+      />
+      <link rel="shortcut icon" href={`/images/favicon/favicon.ico`} />
+      <meta name="msapplication-TileColor" content="#000000" />
+      <meta name="theme-color" content="#1F1D2B" />
+      <meta name="twitter:card" content="summary_medium_image" />
+      <meta name="twitter:site" content="@tangle_network" />
+      <meta name="twitter:creator" content="@webbprotocol" />
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <link rel="canonical" href={fullUrl} />
+      <meta property="og:type" content="website" />
+      <meta
+        property="twitter:image"
+        content="https://assets-global.website-files.com/6494562b44a28080aafcbad4/6494599436915879aa403230_Tangle%20Logo.png"
+      />
+      <meta
+        property="og:image"
+        content="https://assets-global.website-files.com/6494562b44a28080aafcbad4/6494599436915879aa403230_Tangle%20Logo.png"
+      />
+      <meta property="og:locale" content="en_IE" />
+      <meta property="og:site_name" content="Tangle Network" />
+      <link rel="prefetch" href="/" as="document" />
+    </>
+  );
+};
+
 const theme: DocsThemeConfig = {
   docsRepositoryBase: "https://github.com/webb-tools/tangle-docs/tree/main",
   useNextSeoProps() {
@@ -104,20 +171,23 @@ const theme: DocsThemeConfig = {
     component: Navigation,
     extraContent: (
       <>
-        <HelpDiscordBtn />
-        <ThemeSwitch className="hidden border border-gray-200 dark:border-gray-600 mix-blend-plus-lighter sm:block" />
+        <HelpDiscordBtn className="hidden xs:inline-block" />
+        <ThemeSwitch
+          lite
+          className="hidden border border-gray-200 xs:inline-block md:hidden lg:inline-block dark:border-gray-600 mix-blend-plus-lighter"
+        />
       </>
     ),
+  },
+  // Remove React warning in console https://github.com/shuding/nextra/issues/1213#issuecomment-1493038320
+  search: {
+    loading: "Loading...",
   },
   footer: {
     component: Footer,
   },
   nextThemes: {
     defaultTheme: "light",
-  },
-  // Remove React warning in console https://github.com/shuding/nextra/issues/1213#issuecomment-1493038320
-  search: {
-    loading: "Loading...",
   },
   primaryHue: 236,
   primarySaturation: {
